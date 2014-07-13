abd5b97194189c3d53ad3fb26d7b7df6e6f8cdc1
ROSE CASS <rose.cass12@gmail.com>
2014-07-13 13:31:18 -0700

    Did a a bunch of css and work on the reviews index page.RC

--- public_html/wp-config.php
+++ public_html/wp-config.php
@@ -80,11 +80,15 @@ define('WPLANG', '');
  */
 define('WP_DEBUG', false);
 
+define('CONCATENATE_SCRIPTS', false);
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
+
+
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
